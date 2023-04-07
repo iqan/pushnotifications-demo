4f4da0e0df8b28e1f9df7e46e41d09f75cbf998b
iqans <iqan.shaikh@gmail.com>
2023-04-07 16:41:28 +0100

    function fixed

--- AzureFunction/PromoNotificationFunc/NotifyOnPromo.cs
+++ AzureFunction/PromoNotificationFunc/NotifyOnPromo.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
 using Microsoft.Azure.NotificationHubs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
@@ -10,33 +11,26 @@ namespace PromoNotificationFunc
     {
         [FunctionName(nameof(NotifyOnPromo))]
         public static async Task Run([CosmosDBTrigger(
-            databaseName: "iqans-demo-cosmos-db",
-            containerName: "promo",
-            Connection = "CosmosDBConnection",
-            LeaseContainerName = "leases")]IReadOnlyList<PromoItem> input, ILogger log)
+            databaseName: "iqans-demos-cosmos",
+            collectionName: "promo",
+            ConnectionStringSetting = "CosmosDBConnection",
+            CreateLeaseCollectionIfNotExists = true)] IReadOnlyList<Document> input, ILogger log)
         {
             if (input != null && input.Count > 0)
             {
                 log.LogInformation("Documents modified " + input.Count);
-                var promoText = input[0].text;
-                log.LogInformation("Promo Title " + input[0].title);
+                var promoText = input[0].Id;
+                log.LogInformation("Promo Title " + input[0].GetPropertyValue<string>("title"));
                 log.LogInformation("Promo Text " + promoText);
 
                 var connectionString = "<PUT YOUR NOTIFICATION HUB CONNECTION STRING HERE>";
                 var hubName = "iqans-demos-notificationhub";
                 var nhClient = NotificationHubClient.CreateClientFromConnectionString(connectionString, hubName);
 
-                var notificationPayload = "{\"data\":{\"body\":\"" + promoText + "\",\"title\":\"" + input[0].title + "\"}}";
+                var notificationPayload = "{\"data\":{\"body\":\"" + promoText + "\",\"title\":\"" + input[0].Id + "\"}}";
                 var notification = new FcmNotification(notificationPayload);
-
                 var outcomeFcmByTag = await nhClient.SendFcmNativeNotificationAsync(notificationPayload);
             }
         }
-    }
-
-    public class PromoItem
-    {
-        public string title { get; set; }
-        public string text { get; set; }
     }
 }
